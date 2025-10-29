8f82cdd70c97c3e0358b780488f391bd5f1855d8
Veronika Korniushina <vkorniushina@gmail.com>
2025-10-29 12:21:33 +0600

    refactor MainPage to use api

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,20 +1,42 @@
-import React, {useState} from "react";
-import Header from "../../components/Header/Header.jsx";
+import React, {useEffect, useState} from "react";
+import HeaderMain from "../../components/HeaderMain/HeaderMain.jsx";
 import plusIcon from "../../assets/images/plus.svg";
 import styles from "../MainPage/MainPage.module.scss";
 import Card from "../../components/Card/Card.jsx";
-import {tests} from "../../mocks/tests.js";
+import {getAllModules} from "../../api/modules.js";
 
 const MainPage = () => {
+    const [tests, setTests] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
     const [searchQuery, setSearchQuery] = useState('');
 
+    useEffect(() => {
+        const fetchTests = async () => {
+            try {
+                setLoading(true);
+                const data = await getAllModules();
+                setTests(data);
+                setError(null);
+            } catch (err) {
+                setError('Не удалось загрузить тесты');
+                console.error('Error fetching tests:', err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchTests();
+    }, []);
+
+
     const filteredTests = searchQuery.trim()
-        ? tests.filter(test => test.title.toLowerCase().includes(searchQuery.toLowerCase()))
+        ? tests.filter(test => test.name.toLowerCase().includes(searchQuery.toLowerCase()))
         : tests;
 
     return (
         <>
-            <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
+            <HeaderMain searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
             <main className={`container ${styles.main}`}>
                 <div className={styles.headerRow}>
                     <h1 className={styles.title}>Мои тесты</h1>
@@ -24,31 +46,37 @@ const MainPage = () => {
                     </button>
                 </div>
 
-                {filteredTests.length > 0 ? (
-                    <div className={styles.cards}>
-                        {filteredTests.map((test) => (
-                            <Card
-                                key={test.id}
-                                title={test.title}
-                                description={test.description}
-                                questions={test.questions}
-                                progress={test.progress}
-                            />
-                        ))}
-                    </div>
-                ) : (
-                    <div className={styles.emptyState}>
-                        <p className={styles.emptyMessage}>
-                            {searchQuery.trim()
-                                ? "По вашему запросу ничего не найдено"
-                                : "У вас пока нет тестов"}
-                        </p>
-                        <p className={styles.emptySubMessage}>
-                            {searchQuery.trim()
-                                ? "Попробуйте изменить поисковый запрос"
-                                : "Создайте первый тест, чтобы начать обучение"}
-                        </p>
-                    </div>
+                {loading && <div className={styles.loadingEmptyState}>Загрузка тестов...</div>}
+                {error && <div className={styles.loadingEmptyState}>{error}</div>}
+
+                {!loading && !error && (
+                    filteredTests.length > 0 ? (
+                        <div className={styles.cards}>
+                            {filteredTests.map((test) => (
+                                <Card
+                                    key={test.id}
+                                    id={test.id}
+                                    name={test.name}
+                                    description={test.description}
+                                    questionsCount={test.questionsCount}
+                                    progress={test.progress}
+                                />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className={styles.emptyState}>
+                            <p className={styles.emptyMessage}>
+                                {searchQuery.trim()
+                                    ? "По вашему запросу ничего не найдено"
+                                    : "У вас пока нет тестов"}
+                            </p>
+                            <p className={styles.emptySubMessage}>
+                                {searchQuery.trim()
+                                    ? "Попробуйте изменить поисковый запрос"
+                                    : "Создайте первый тест, чтобы начать обучение"}
+                            </p>
+                        </div>
+                    )
                 )}
             </main>
         </>
