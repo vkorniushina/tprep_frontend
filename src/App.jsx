540e01066657d93fdcfaf268ebf3a634d61a0461
Veronika Korniushina <vkorniushina@gmail.com>
2025-10-29 13:33:20 +0600

    implement router setup in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import MainPage from './pages/MainPage/MainPage';
+import TestPage from "./pages/TestPage/TestPage.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
     return (
-        <>
-            <MainPage />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/test/:id" element={<TestPage />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
