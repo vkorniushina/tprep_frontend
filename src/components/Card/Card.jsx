4504ec7125633596a8804c027c4f4845bfe861b4
Veronika Korniushina <vkorniushina@gmail.com>
2025-10-29 12:22:11 +0600

    update Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,28 +2,34 @@ import styles from './Card.module.scss'
 import fileIcon from "../../assets/images/file.svg";
 import React from "react";
 
-const Card = ({title, description, questions, progress}) => {
+const Card = ({id, name, description, questionsCount, progress}) => {
+
+    const percent = questionsCount === 0 ? 0 : (progress / questionsCount) * 100;
+    const displayPercent = Math.round(percent);
+
     return (
         <div className={styles.card}>
             <div className={styles.topContent}>
-                <h3 className={styles.title}>{title}</h3>
+                <h3 className={styles.name}>{name}</h3>
                 {description ? <p className={styles.description}>{description}</p> :
                     <p className={styles.description} style={{color: '#a6a6a6'}}>Здесь могло быть описание...</p>}
             </div>
             <div className={styles.bottomContent}>
                 <div className={styles.amount}>
                     <img src={fileIcon} alt="Doc"/>
-                    <span>{questions} вопросов</span>
+                    <span>{questionsCount} вопросов</span>
                 </div>
                 <div className={styles.row}>
-                    <div className={styles.progress}><span className={styles.bar} style={{width: `${progress}%`}}/>
+                    <div className={styles.progress}><span className={styles.bar} style={{width: `${displayPercent}%`}}/>
                     </div>
-                    <span>{progress}%</span>
+                    <span>{displayPercent}%</span>
                 </div>
                 <button className={styles.openBtn}>Открыть тест</button>
             </div>
         </div>
+
     )
 }
 
 export default Card;
+
