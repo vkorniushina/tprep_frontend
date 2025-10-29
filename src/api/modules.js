a0ba6ee3b66fcacc33e0f537e76a244ba32f925e
Veronika Korniushina <vkorniushina@gmail.com>
2025-10-29 13:36:56 +0600

    add get module by id api method

--- src/api/modules.js
+++ src/api/modules.js
@@ -9,3 +9,13 @@ export const getAllModules = async () => {
         throw error;
     }
 };
+
+export const getModuleById = async (id) => {
+    try {
+        const response = await apiClient.get(`/modules/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error fetching module ${id}:`, error);
+        throw error;
+    }
+};
