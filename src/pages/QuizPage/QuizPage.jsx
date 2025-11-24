6d91e4d9d00faa606cab8dbae60c8a56e924d413
Veronika Korniushina <vkorniushina@gmail.com>
2025-11-24 20:39:12 +0600

    refactor modal scroll lock

--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -145,15 +145,9 @@ const QuizPage = () => {
 
     useEffect(() => {
         if (showResultModal) {
-            const scrollY = window.scrollY;
-            document.body.style.position = 'fixed';
-            document.body.style.top = `-${scrollY}px`;
-            document.body.style.width = '100%';
+            document.body.style.overflow = "hidden";
         } else {
-            const scrollY = document.body.style.top;
-            document.body.style.position = '';
-            document.body.style.top = '';
-            window.scrollTo(0, parseInt(scrollY || '0') * -1);
+            document.body.style.overflow = "";
         }
     }, [showResultModal]);
 
