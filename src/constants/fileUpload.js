1ce7a9bbc1ecc0f30f96f93e97def356626541a4
Veronika Korniushina <vkorniushina@gmail.com>
2026-01-10 20:06:47 +0600

    add file upload constants and formatFileSize utility

---
+++ src/constants/fileUpload.js
@@ -0,0 +1,9 @@
+export const MAX_FILE_SIZE = 5 * 1024 * 1024;
+
+export const ALLOWED_FILE_TYPES = [
+    "text/plain",
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+];
+
+export const ALLOWED_FILE_EXTENSIONS = ['txt', 'doc', 'docx'];
