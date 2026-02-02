5157736a0d6e18ca365d7b91ded2659ff9817f32
Veronika Korniushina <vkorniushina@gmail.com>
2026-02-03 00:55:21 +0500

    extract OptionItem component

--- src/components/EditQuestionBlock/EditQuestionBlock.jsx
+++ src/components/EditQuestionBlock/EditQuestionBlock.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import styles from "./EditQuestionBlock.module.scss";
 import trash from "../../assets/images/trash.svg";
-import tick from "../../assets/images/tick.svg";
-import Cross from "../../assets/images/cross.svg?react";
 import AddIcon from "../../assets/images/add.svg?react";
 import {QUESTION_TYPES} from "../../constants/questionTypes";
 import {QUESTION_FIELDS} from "../../constants/questionFields";
 import classNames from 'classnames';
 import { v4 as uuidv4 } from 'uuid';
+import OptionItem from "../OptionItem/OptionItem.jsx";
 
 const EditQuestionBlock = ({index, data, onUpdate, onDelete, errors}) => {
     const updateField = (field, value) => {
@@ -18,6 +17,15 @@ const EditQuestionBlock = ({index, data, onUpdate, onDelete, errors}) => {
         const updated = [...data.options];
         updated[idx] = {...updated[idx], [QUESTION_FIELDS.CONTENT]: value};
         updateField(QUESTION_FIELDS.OPTIONS, updated);
+    };
+
+    const toggleOptionCorrect = (idx) => {
+        const updatedOptions = data.options.map((item, i) =>
+            i === idx
+                ? {...item, [QUESTION_FIELDS.IS_CORRECT]: !item.isCorrect}
+                : item
+        );
+        updateField(QUESTION_FIELDS.OPTIONS, updatedOptions);
     };
 
     const addOption = () => {
@@ -153,47 +161,16 @@ const EditQuestionBlock = ({index, data, onUpdate, onDelete, errors}) => {
                             </div>
 
                             <div className={styles.optionsList}>
-                                {data.options && data.options.map((ans, idx) => (
-                                    <div key={ans.id} className={styles.optionItem}>
-                                        <div className={styles.optionContentWrapper}>
-                                            <label className={styles.checkboxLabel}>
-                                                <input
-                                                    type="checkbox"
-                                                    className={styles.checkboxInput}
-                                                    checked={ans.isCorrect}
-                                                    onChange={() => {
-                                                        const updatedOptions = data.options.map((item, i) =>
-                                                            i === idx
-                                                                ? {...item, [QUESTION_FIELDS.IS_CORRECT]: !item.isCorrect}
-                                                                : item
-                                                        );
-                                                        updateField(QUESTION_FIELDS.OPTIONS, updatedOptions);
-                                                    }}
-                                                />
-                                                <span className={styles.checkboxSquare}>
-                                                {ans.isCorrect && (
-                                                    <img src={tick} alt="Выбрано" className={styles.checkboxTick}/>
-                                                )}
-                                                </span>
-                                            </label>
-
-                                            <input
-                                                className={classNames(
-                                                    styles.optionInput,
-                                                    errors.optionContent?.[idx]?.content && styles.inputError
-                                                )}
-                                                value={ans.content}
-                                                onChange={(e) => updateOption(idx, e.target.value)}
-                                            />
-
-                                            <button className={styles.optionRemove} onClick={() => removeOption(idx)}>
-                                                <Cross className={styles.cross}/>
-                                            </button>
-                                        </div>
-                                        {errors.optionContent?.[idx]?.content && (
-                                            <div className={styles.errorMessageFlow}>{errors.optionContent[idx].content}</div>
-                                        )}
-                                    </div>
+                                {data.options && data.options.map((option, idx) => (
+                                    <OptionItem
+                                        key={option.id}
+                                        option={option}
+                                        index={idx}
+                                        onContentChange={(value) => updateOption(idx, value)}
+                                        onToggleCorrect={() => toggleOptionCorrect(idx)}
+                                        onRemove={() => removeOption(idx)}
+                                        error={errors.optionContent?.[idx]?.content}
+                                    />
                                 ))}
                             </div>
 
