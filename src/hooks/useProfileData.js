73d4a05b33a00ed06ccf3f43d2b41948f0c481e9
Veronika Korniushina <vkorniushina@gmail.com>
2026-03-27 16:50:10 +0600

    implement useProfileData hook

---
+++ src/hooks/useProfileData.js
@@ -0,0 +1,26 @@
+import {useState, useEffect} from "react";
+import {getProfile} from "../api/profile.js";
+
+export const useProfileData = () => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const data = await getProfile();
+                setUser(data.user);
+            } catch (err) {
+                setError("Не удалось загрузить данные профиля");
+                console.error("Error fetching profile:", err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProfile();
+    }, []);
+
+    return {user, loading, error};
+}
