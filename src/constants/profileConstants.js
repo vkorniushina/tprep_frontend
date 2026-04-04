b0c6786981addcff53db519ae95ca85627a4a6b9
Veronika Korniushina <vkorniushina@gmail.com>
2026-04-04 11:10:07 +0600

    add profile constants and api methods

---
+++ src/constants/profileConstants.js
@@ -0,0 +1,14 @@
+export const PROFILE_TABS = {
+    STATS: 'stats',
+    REMINDERS: 'reminders',
+};
+
+export const ACTIVITY_MODES = {
+    WEEK: 'week',
+    YEAR: 'year',
+};
+
+export const EDIT_STEPS = {
+    FORM: "form",
+    VERIFYING: "verifying"
+};
