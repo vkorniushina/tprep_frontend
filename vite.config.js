25eca45f98f1be264c1cb8b24f93d45d388d91d2
Veronika Korniushina <vkorniushina@gmail.com>
2026-07-11 13:40:22 +0600

    chore: setup testing environment

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
         react(),
         svgr(),
     ],
+    test: {
+        environment: 'jsdom',
+        setupFiles: './src/test/setup.js',
+        globals: true,
+    },
 })
